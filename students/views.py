096e2680d987e80b1461e0056fb4abe504acf10a
maksim boiar <draakan79@gmail.com>
2016-10-16 12:42:30 +0300

    otake vono

--- students/views.py
+++ students/views.py
@@ -34,7 +34,20 @@ def students_delete(request, sid):
 	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups = (
+		{'id': 1,
+		  'name': u'МтМ-21',
+		  'leader': {'id': 1, 'name': u'Тарас Мельник'}},
+		{'id': 2,
+		  'name': u'Мтм-22',
+		  'leader': {'id': 2, 'name': u'Микола Садовський'}},
+		{'id': 3,
+		  'name': u'Мтм-23',
+		  'leader': {'id': 3, 'name': u'Іванов Андрій'}},
+		  
+	)	
+	return render(request, 'students/group_list.html', 
+		{'groups': groups})
 	
 def groups_add(request):
 	return HttpResponse('<h1>Group Add Form</h1>')
